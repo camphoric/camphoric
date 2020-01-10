c048bdd11bf1e3ee9f9618c3e38062d2ffcb6365
Evin Sellin <evinism@gmail.com>
2020-01-09 21:41:21 -0800

    Add basic csr

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,8 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import RegisterPage from "../RegisterPage";
 
-const App = () => <RegisterPage />;
+const App = () => (
+  <Router>
+    <Route path="/" exact={true}>
+      <h1>Camphoric!</h1>
+      <Link to="/register">Registration Page</Link>
+    </Route>
+    <Route path="/register">
+      <RegisterPage />
+    </Route>
+  </Router>
+);
 
 export default App;
