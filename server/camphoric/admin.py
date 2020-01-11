6b90535ee3522809e02389396fcf6e3b850851d3
Evin Sellin <evinism@gmail.com>
2020-01-10 16:45:08 -0800

    add org to admin panel because i forgot to before

--- server/camphoric/admin.py
+++ server/camphoric/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Event, Registration, Lodging, Camper, Deposit, Payment
+from .models import Event, Registration, Lodging, Camper, Deposit, Payment, Organization
 
 admin.site.register(Event)
 admin.site.register(Registration)
@@ -7,3 +7,4 @@ admin.site.register(Lodging)
 admin.site.register(Camper)
 admin.site.register(Deposit)
 admin.site.register(Payment)
+admin.site.register(Organization)
