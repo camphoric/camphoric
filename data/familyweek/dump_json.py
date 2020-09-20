e19eeb0e369067c425138d6a2cca2b47617d00d9
Ben Saylor <brsaylor@gmail.com>
2020-09-20 14:12:14 -0700

    Update dump_json script

--- data/familyweek/dump_json.py
+++ data/familyweek/dump_json.py
@@ -1,12 +1,22 @@
 import json
-import camper_schema
+
 import camper_pricing_logic
+import camper_schema
+import confirmation_email_template
 import pricing
 import registration_pricing_logic
 import registration_schema
 import registration_ui_schema
 
 
-for module in [registration_schema, camper_schema, registration_ui_schema]:
-	with open(module.__name__ +'.json', 'w') as f:
-		json.dump(module.data, f, indent=4)
+for module in [
+        camper_pricing_logic,
+        camper_schema,
+        confirmation_email_template,
+        pricing,
+        registration_pricing_logic,
+        registration_schema,
+        registration_ui_schema,
+]:
+    with open(module.__name__ +'.json', 'w') as f:
+        json.dump(module.data, f, indent=4)
