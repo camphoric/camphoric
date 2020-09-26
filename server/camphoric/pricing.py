1f98dc64a23c594863360f80a685e58e59cd4e91
Zia Rauwolf <toolgirl@users.noreply.github.com>
2020-09-26 12:28:11 -0700

    Pass dates as objects to JSONLogic (#60)
    * Pass event dates to jsonLogic
    
    * Parse camper dates for pricing logic
    
    * Include parsed event dates with register response

--- server/camphoric/pricing.py
+++ server/camphoric/pricing.py
@@ -1,4 +1,6 @@
 from collections import defaultdict
+import numbers
+
 from json_logic import jsonLogic
 
 
@@ -44,7 +46,11 @@ def calculate_price(registration, campers):
     data = {
         "registration": registration.attributes,
         "pricing": event.pricing,
+        "event": get_event_attributes(event),
     }
+
+    date_props = get_date_props(event.camper_schema)
+
     for reg_component in event.registration_pricing_logic:
         var = reg_component["var"]
         value = jsonLogic(reg_component["exp"], data)
@@ -52,16 +58,54 @@ def calculate_price(registration, campers):
         data[var] = value
 
     for camper in campers:
-        data["camper"] = camper.attributes
-        camper_results = {}
+        data["camper"] = camper.attributes.copy()
+        for date_prop in date_props:
+            data["camper"][date_prop] = datestring_to_dict(data["camper"][date_prop])
 
+        camper_results = {}
         for camper_component in event.camper_pricing_logic:
             var = camper_component["var"]
             value = jsonLogic(camper_component["exp"], data)
             camper_results[var] = value
-            results[var] += value
+            if isinstance(value, numbers.Number):
+                results[var] += value
             data[var] = value
 
         results['campers'].append(camper_results)
 
     return dict(results)
+
+
+def datetime_to_dict(d):
+    return {
+        "year": d.year,
+        "month": d.month,
+        "day": d.day,
+    }
+
+
+def datestring_to_dict(s):
+    y, m, d = [int(x) for x in s.split("-")]
+    return {
+        "year": y,
+        "month": m,
+        "day": d,
+    }
+
+
+def get_date_props(camper_schema):
+    if not camper_schema:
+        return []
+    date_props = []
+    for prop_name, prop_schema in camper_schema["properties"].items():
+        if prop_schema.get("type") == "string" and prop_schema.get("format") == "date":
+            date_props.append(prop_name)
+    return date_props
+
+
+def get_event_attributes(event):
+    attributes = {}
+    for field in ["registration_start", "registration_end", "start", "end"]:
+        if getattr(event, field):
+            attributes[field] = datetime_to_dict(getattr(event, field))
+    return attributes
