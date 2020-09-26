0df3c65205de1b75505c0ff5a4d0c33ec33d7ec0
Will Wheeler <willfulbard@gmail.com>
2020-09-26 12:41:45 -0700

    Add codesplitting and admin interface

---
+++ client/src/components/AdminPage/AdminPage.tsx
@@ -0,0 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import React from "react";
+
+import "./AdminPage.css";
+
+
+class App extends React.Component {
+  constructor(props = {}) {
+    super(props);
+  }
+
+  render() {
+    return <div className="App">admin interface</div>;
+  }
+}
+
+export default App;
--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,17 +1,28 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import React from "react";
+import React, { Suspense } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Spinner from "../Spinner";
 
-import RegisterPage from "../RegisterPage";
+const AdminPage = React.lazy(() => import('../AdminPage'));
+const RegisterPage = React.lazy(() => import('../RegisterPage'));
 
 const App = () => (
   <Router>
     <Route path="/" exact={true}>
       <h1>Camphoric!</h1>
-      <Link to="/register">Registration Page</Link>
+      <p><Link to="/register">Registration Page</Link></p>
+      <p><Link to="/admin">Admin Page</Link></p>
     </Route>
     <Route path="/register">
-      <RegisterPage />
+      <Suspense fallback={<Spinner />}>
+        <RegisterPage />
+      </Suspense>
+    </Route>
+    <Route path="/admin">
+      <Suspense fallback={<Spinner />}>
+        <AdminPage />
+      </Suspense>
+
     </Route>
   </Router>
 );
