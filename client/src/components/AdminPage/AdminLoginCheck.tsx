bfec3d07f9c9ee88c21fbfc207cfbd83461c8271
Will Wheeler <willfulbard@gmail.com>
2020-10-28 20:35:12 -0700

    Create admin page with basic login

---
+++ client/src/components/AdminPage/AdminLoginCheck.tsx
@@ -0,0 +1,66 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import React, { Suspense } from "react";
+import Spinner from "../Spinner";
+
+import "./AdminPage.css";
+
+const Login = React.lazy(() => import('../Login'));
+const AdminPage = React.lazy(() => import('./AdminPage'));
+
+type AdminLoginCheckState = {
+  authToken?: string,
+  organizations?: Array<any>,
+};
+
+class AdminLoginCheck extends React.Component {
+  state: AdminLoginCheckState = {
+    organizations: undefined,
+    authToken: undefined,
+  };
+
+  async getOrganizations() {
+    let organizations
+    try {
+      const response = await fetch(
+        '/api/organizations/',
+      );
+
+      organizations = await response.json();
+    } catch (e) {
+
+    }
+    this.setState({ organizations });
+  }
+
+  render() {
+    let pageContent: JSX.Element;
+
+    if (!this.state.authToken) {
+      pageContent = (
+        <section className="Login">
+          <Suspense fallback={<Spinner />}>
+            <Login
+              onLoginSuccess={(authToken) => this.setState({ authToken })}
+              onLoginFail={(e) => { throw e; }}
+            />
+          </Suspense>
+        </section>
+      );
+    } else {
+      pageContent = (
+        <section className="AdminPage">
+          <Suspense fallback={<Spinner />}>
+            <AdminPage
+              authToken={this.state.authToken}
+            />
+          </Suspense>
+        </section>
+      );
+    }
+
+    return <div className="App">{pageContent}</div>;
+  }
+}
+
+export default AdminLoginCheck;
--- client/src/components/Spinner/Spinner.tsx
+++ client/src/components/Spinner/Spinner.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import './Spinner.css';
 
-const Spinner = ({text = 'Loading'}) => (
+const Spinner = ({text = 'Loading', color = 'antiquewhite'}) => (
   <div className="spinner">
     <svg viewBox="-10 -10 70 70">
       <mask id="myMask">
         <rect x="0" y="0" width="50" height="50" fill="aliceblue" />
         <path d="M25,25 L0,-25 L0,25 Z" fill="black" />
       </mask>
-      <circle cx="25" cy="25" r="20" mask="url(#myMask)" stroke="antiquewhite" fill="none" strokeWidth="5" />
+      <circle cx="25" cy="25" r="20" mask="url(#myMask)" stroke={color} fill="none" strokeWidth="5" />
     </svg>
-    {text}
+    <div style={{ color: color }}>{text}</div>
   </div>
 );
 
