1b7c96242fea4553c8d937210fe8f8385c52632a
Ben Saylor <brsaylor@gmail.com>
2020-10-31 11:54:11 -0700

    Update client calculatePrice except for date logic

--- client/src/components/RegisterPage/utils.ts
+++ client/src/components/RegisterPage/utils.ts
@@ -1,75 +1,61 @@
 import jsonLogic from "json-logic-js";
-import { RegistrationState } from "./RegisterPage";
+import { JSONSchema6 } from "json-schema";
 
-export function calculatePrice(state: RegistrationState) {
+import { RegistrationConfig, FormData } from "./RegisterPage";
+
+type PricingData = {
+  event: { [key: string]: any };
+  registration: FormData;
+  pricing: { [key: string]: any };
+  [key: string]: any;
+};
+
+type PricingResults = {
+  [key: string]: any;
+  campers: Array<{ [key: string]: any }>;
+  total: number;
+};
+
+export function calculatePrice(config: RegistrationConfig, formData: FormData) {
   // calculation should be done in whole dollars for sake of
   // avoiding funky issues with floats. If sub-dollar amounts
   // are necessary, we should switch this to cents.
-  if (state.status === "fetching" || !state.formData) return { total: 0 };
 
-  const pricingLogic = state.config.pricingLogic;
+  const { event, pricingLogic, pricing } = config;
 
-  const data = {
-    registration: state.formData,
-    pricing: state.config.pricing
+  const results: PricingResults = {
+    campers: [],
+    total: 0,
   };
 
-  const campers = (state.formData.campers || []).map(camper =>
-    jsonLogic.apply(pricingLogic.camper, {
-      ...data,
-      camper
-    })
-  );
-
-  const meals = (state.formData.campers || []).map(camper =>
-    jsonLogic.apply(pricingLogic.meals, {
-      ...data,
-      camper
-    })
-  );
-
-  const parking = jsonLogic.apply(pricingLogic.parking, data);
-
-  const shirts = jsonLogic.apply(pricingLogic.shirts, data);
-
-  const donation = jsonLogic.apply(pricingLogic.donation, data);
-
-  const total =
-    campers.reduce((t, c) => c + t) +
-    meals.reduce((t, c) => c + t) +
-    parking +
-    shirts +
-    donation;
-
-  const result = {
-    campers,
-    meals,
-    parking,
-    shirts,
-    donation,
-    total
+  const data: PricingData = {
+    event,
+    registration: formData,
+    pricing,
   };
 
-  Object.entries({
-    parking,
-    shirts,
-    donation,
-    total,
-    ...campers.reduce((o, c, k) => ({ ...o, [`campers${k}`]: c }), {}),
-    ...meals.reduce((o, c, k) => ({ ...o, [`meals${k}`]: c }), {})
-  }).forEach(([key, value]) => {
-    if (typeof value !== "number") {
-      console.log(result);
-      throw new Error(
-        `Pricing returned incorrect type for ${key} (expected number, got ${typeof value})`
-      );
-    } else if (Math.floor(value) !== value) {
-      console.log(result);
-      throw new Error(
-        `Pricing returned non-natural number for ${key} (got ${value}).`
-      );
-    }
+  pricingLogic.registration.forEach(component => {
+    const varName = component.var;
+    const value = jsonLogic.apply(component.exp, data);
+    results[varName] = isNaN(value) ? 0 : value;
+    data[varName] = value;
   });
 
-  return result;
+  formData.campers.forEach(camper => {
+    data.camper = camper;
+    const camperResults: { [key: string]: any } = {};
+    pricingLogic.camper.forEach(component => {
+      const varName = component.var;
+      const value = jsonLogic.apply(component.exp, data);
+      camperResults[varName] = value;
+      if (typeof value === 'number' || typeof value === 'boolean') {
+        const subtotal = (results[varName] || 0) + value;
+        results[varName] = subtotal;
+        data[varName] = value;
+      }
+    });
+    results.campers.push(camperResults);
+  });
+
+  return results;
 }
