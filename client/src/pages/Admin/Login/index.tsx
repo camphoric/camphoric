d61e82eb141fff52273502374257f9fd6ad05010
Will Wheeler <willfulbard@gmail.com>
2021-09-15 15:10:49 -0700

    Move AdminPage and RegisterPage to Pages

--- client/src/pages/Admin/Login/index.tsx
+++ client/src/pages/Admin/Login/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 
-import Spinner from '../../Spinner';
+import Spinner from 'components/Spinner';
 
 /**
  * A login form
