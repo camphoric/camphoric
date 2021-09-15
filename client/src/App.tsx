ea7f7f7e6436427db56dbf72cf7f34685bd947e3
Will Wheeler <willfulbard@gmail.com>
2021-09-15 15:31:42 -0700

    Simplify App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Spinner from 'components/Spinner';
 import StateProvider from 'hooks/Provider';
 
-import Splash from './Splash';
+import Default from 'components/Default';
 
 const AdminPage = React.lazy(() => import('pages/Admin'));
 const RegisterPage = React.lazy(() => import('pages/Register'));
@@ -13,7 +13,7 @@ const App = () => (
     <Suspense fallback={<Spinner />}>
       <Router>
         <Route path="/" exact={true}>
-          <Splash />
+          <Default />
         </Route>
         <Route path="/events/:eventId/register">
           <RegisterPage />
