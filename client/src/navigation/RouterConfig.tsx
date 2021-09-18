0ef12320eb2e16d7b6de9b276715086f2045b141
Will Wheeler <willfulbard@gmail.com>
2021-09-18 15:33:36 -0700

    Move EventAdmin

--- client/src/navigation/RouterConfig.tsx
+++ client/src/navigation/RouterConfig.tsx
@@ -3,49 +3,34 @@ import {
   Switch,
   Route,
   Redirect,
-  useRouteMatch,
   useLocation,
 } from 'react-router-dom';
 
 import Default from 'components/Default';
 
-const AdminPage = React.lazy(() => import('pages/Admin'));
 const RegisterPage = React.lazy(() => import('pages/Register'));
 
-const OrganizationChooser = React.lazy(() => import('pages/Admin/OrganizationChooser'));
-const EventChooser = React.lazy(() => import('pages/Admin/EventChooser'));
-const EventAdmin = React.lazy(() => import('pages/Admin/EventAdmin'));
+const OrganizationChooser = React.lazy(() => import('pages/OrganizationChooser'));
+const EventChooser = React.lazy(() => import('pages/EventChooser'));
+const EventAdmin = React.lazy(() => import('pages/EventAdmin'));
 
 const RouterConfig = () => {
-  const { url } = useRouteMatch();
   const { pathname } = useLocation();
 
   return (
     <Switch>
       <Redirect from="/:url*(/+)" to={pathname.slice(0, -1)} />
-      <Route path="/" exact={true}>
-        <Default />
-      </Route>
-      <Route path="/events/:eventId/register">
-        <RegisterPage />
-      </Route>
-      <Route path="/admin">
-        <AdminPage />
-      </Route>
+      <Route exact path="/" component={Default} />
+      <Route exact path="/events/:eventId/register" component={RegisterPage} />
+      <Redirect exact from="/admin" to="/admin/organization" />
 
-      <Route path='/admin/organization/:organizationId/event/:eventId'>
-        <EventAdmin />
-      </Route>
-      <Route path='/admin/organization/:organizationId/event'>
-        <EventChooser />
-      </Route>
-      <Route path='/admin/organization'>
-        <OrganizationChooser />
-      </Route>
-      <Route path="/admin/organization/:organizationId">
-        <Redirect from={url} to={`${url}/event`} />
-      </Route>
-      <Redirect from={url} to={`${url}/organization`} />
+      <Route exact path="/admin/organization" component={OrganizationChooser} />
+      <Redirect exact
+        from="/admin/organization/:organizationId"
+        to="/admin/organization/:organizationId/event"
+      />
+      <Route exact path="/admin/organization/:organizationId/event" component={EventChooser} />
+      <Route path="/admin/organization/:organizationId/event/:eventId" component={EventAdmin} />
     </Switch>
   );
 };
