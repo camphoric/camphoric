2b085f9dbe50c5570a9d95611254764d3efe7906
Will Wheeler <willfulbard@gmail.com>
2022-01-27 22:29:49 -0800

    Refine meal pricing logic

---
+++ data/lark/camperMeals.mjs
@@ -0,0 +1,80 @@
+const dependencies = {
+  "meal_plan": {
+    "oneOf": [
+      {
+        "properties": {
+          "meal_plan": {
+            "enum": [""]
+          }
+        }
+      },
+      {
+        "properties": {
+          "meal_plan": {
+            "enum": [
+              "F",
+              "D",
+              "A",
+              "B"
+            ]
+          },
+          "meal_type": {
+            "type": "string",
+            "title": "What types of meals do you want?",
+            "enumNames": [
+              "Non-Vegetarian",
+              "Vegetarian",
+              "Vegan"
+            ],
+            "enum": [
+              "O",
+              "V",
+              "E"
+            ]
+          }
+        }
+      }
+    ]
+  }
+};
+
+const lookup = {
+  "F": "All Meals",
+  "D": "Just Dinners",
+  "A": "All Meals (first half)",
+  "B": "All Meals (second half)",
+}
+
+const createMeals = (...options) => ({
+  "type": "object",
+  "title": "Meal Plans",
+  "description": `Meal plans must be fully paid {meals_due_date}. We cannot accept partial payments. Food prices include sales tax. 
+
+NOTE: You may only cook if you’re camping in a vehicle with a built-in kitchen. No portable white gas or propane stoves are allowed in the park.
+
+| Meal Plan Pricing | adult age 12+ | child ages 0-11 |
+| --- | --- | --- |
+| Full camp, full meals   | \${{pricing.meals_adult_full}} adults    | \${{pricing.meals_teen_full}} kids |
+| Full camp, just dinners | \${{pricing.meals_adult_dinners}} adults | \${{pricing.meals_teen_dinners}} kids |
+| Half camp, full meals   | \${{pricing.meals_adult_half}} adults    | \${{pricing.meals_teen_half}} kids |
+
+Meal plans offer significant savings. You may buy individual meals at camp instead of buying a meal plan. (But it costs more!)`,
+  "properties": {
+    "meal_plan": {
+      "type": "string",
+      "title": "What meals will you purchase?",
+      "enumNames": [
+        "None",
+        ...options.map(o => lookup[o])
+      ],
+      "enum": [
+        "",
+        ...options,
+      ],
+      "default": ""
+    }
+  },
+  "dependencies": dependencies,
+});
+
+export default createMeals;
