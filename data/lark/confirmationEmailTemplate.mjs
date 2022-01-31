42f786da6f583c99b1127aa7ef487e61e24d0006
Will Wheeler <willfulbard@gmail.com>
2022-01-30 23:13:56 -0800

    Add markdown table to confirmation email

--- data/lark/confirmationEmailTemplate.mjs
+++ data/lark/confirmationEmailTemplate.mjs
@@ -8,39 +8,16 @@ Your online registration for Lark Camp 2020 has been received. This is an acknow
 You have registered for:
 
 
-| Name | Duration | Tuition | Meals | Total |
-| ---- | -------- | ------- | ----- | ----- |
+| Name | Age | Session | Tuition | Meals | Total |
+| ---- | --- | ------- | ------- | ----- | ----- |
 {{#campers}}
-| {{first_name}} {{last_name}} | {{session}} | {{pricing_result.tuition}} | {{pricing_result.meals}} ({{meals.meal_plan}}) | {{pricing_result.total}} |
+| {{first_name}} {{last_name}} | {{age}} | {{session}} | \${{pricing_result.tuition}} | \${{pricing_result.meals}} ({{meals.meal_plan}}) | \${{pricing_result.total}} |
 {{/campers}}
 
-
-Name - Full/Half Camp
-
-50% Tuition $395 - Full Meals $405
-
 Donation to Lark Camp - None Yet
-
-Parking Pass Purchase $65
-
-
+Parking Pass Purchase - None Yet
 
 TOTAL DUE: \${{pricing_results.total}}
 
-Invoice Number: 108698
-
-
-
-We will send a separate e-mail with instructions for completing payment via PayPal.
-
-
-
-We try to fulfill everyone's first choice of lodging whenever possible.
-
-
-
-Thank you for sending in your registration for Lark Camp.
-
-
-etc.
+Registration Number: 2022-{{ registration.id }}
 `;
