522c659237c4ffa21d1717e39cc5f68388ffefc9
Will Wheeler <willfulbard@gmail.com>
2022-05-03 22:29:59 -0700

    Update schema editor to use new api hooks

--- client/src/pages/EventAdminSettings/EventAdminSettings.tsx
+++ client/src/pages/EventAdminSettings/EventAdminSettings.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { Tabs, Tab } from 'react-bootstrap';
-import { useEvent } from 'hooks/admin';
+import { useEvent } from 'store/hooks';
 import Spinner from 'components/Spinner';
 import { formatDateTimeForApi } from 'utils/time';
 
@@ -9,11 +9,7 @@ import EditSchemaTab from './EditSchemaTab';
 import EditRegistrationTypes from './EditRegistrationTypes';
 
 export interface TabProps {
-  handleFormChange: (field: string) => (changeEvent: React.ChangeEvent<HTMLInputElement>) => any;
-  handleFormDateChange: (field: string) => (changeEvent: React.ChangeEvent<HTMLInputElement>) => any;
-  handleChange: (field: string) => (value: any) => any;
   event: ApiEvent;
-  save: () => any;
 }
 
 type EditableSchemaConfig = {
@@ -34,43 +30,12 @@ export const editableSchemas = {
 export type EditableSchemaKeys = keyof typeof editableSchemas;
 
 function EventAdminHome({ event: originalEvent }: EventAdminPageProps) {
-  const [event, setEvent] = React.useState<ApiEvent>(originalEvent);
-  const { set: apiPutEvent } = useEvent(originalEvent.id);
+  const eventApi = useEvent();
 
-  if (!originalEvent) return <Spinner />;
-
-  // console.log(event);
-
-  const handleFormChange = (field: string) => (changeEvent: React.ChangeEvent<HTMLInputElement>) => setEvent({
-    ...event,
-    [field]: changeEvent.target.value,
-  });
-
-  const handleFormDateChange = (field: string) => (changeEvent: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = changeEvent.target;
-    const dateValue = formatDateTimeForApi(value);
-
-    // console.log(dateValue);
-
-    setEvent({
-      ...event,
-      [field]: dateValue,
-    });
-  }
-
-  const handleChange = (field: string) => (value: any) => setEvent({
-    ...event,
-    [field]: value,
-  });
-
-  const save = () => apiPutEvent(event);
+  if (eventApi.isLoading || !eventApi.data) return <Spinner />;
 
   const tabProps: TabProps = {
-    handleFormChange,
-    handleFormDateChange,
-    handleChange,
-    event,
-    save,
+    event: eventApi.data,
   };
 
   return (
