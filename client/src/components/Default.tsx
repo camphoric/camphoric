5d32c9acf39a9415d24120fa2de1918f7df941d7
Will Wheeler <willfulbard@gmail.com>
2022-06-26 17:43:05 -0700

    Add frontend for event list

--- client/src/components/Default.tsx
+++ client/src/components/Default.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Row, Col } from 'react-bootstrap';
+import Spinner from 'components/Spinner';
+
+type EventListItem = {
+  name: string,
+  open: boolean,
+  url: string,
+};
 
 function Splash(props: {}) {
+  const [eventList, setEventList] = React.useState<Array<EventListItem> | void>();
+
+  React.useEffect(() => {
+    fetch('/api/eventlist')
+      .then(r => r.json())
+      .then(setEventList);
+  }, []);
+
+  if (!eventList) return <Spinner />;
+
   return (
     <Container><Row className="justify-content-md-center"><Col>
       <h1>Camphoric!</h1>
-      <p><Link to="/events/1/register">Registration Page (event 1)</Link></p>
+      {
+        eventList.map((event: EventListItem) => (
+          <p key={event.url}>
+            <Link to={event.url}>{event.name} {!event.open ? "(closed)" : ""}</Link>
+          </p>
+        ))
+      }
       <p><Link to="/admin">Admin Page</Link></p>
     </Col></Row></Container>
   );
