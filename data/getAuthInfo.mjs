049ad6a64cd554b2ff7c0f34c6f774bd205a6ebc
Will Wheeler <willfulbard@gmail.com>
2022-08-10 22:52:46 -0700

    Remove unused auth function in data import

--- data/getAuthInfo.mjs
+++ data/getAuthInfo.mjs
@@ -2,70 +2,6 @@ import fetch from 'node-fetch';
 
 const urlBase = process.env.CAMPHORIC_URL || 'http://django:8000';
 
-function extractCookieString(response) {
-  return response.headers.raw()['set-cookie'].map(
-    c => {
-      const m = c.match(/^([^=]+=[^;]+)/);
-
-      return m[1];
-    }
-  ).join('; ');
-}
-
-async function getAuthInfo() {
-  let username = process.env.DJANGO_SUPERUSER_USERNAME;
-  let password = process.env.DJANGO_SUPERUSER_PASSWORD;
-
-  if (!username && !password) {
-    const answers = await inquirer.prompt([
-      { name: 'username' },
-      { name: 'password', type: 'password' },
-    ]);
-
-    username = answers.username;
-    password = answers.password;
-  }
-
-  let response;
-  let cookies;
-  let match;
-  let csrf;
-  let rawHeaders;
-
-  response = await fetch(`${urlBase}/api/set-csrf-cookie`);
-  rawHeaders = response.headers.raw();
-  // console.log('rawHeaders', rawHeaders);
-  cookies = rawHeaders['set-cookie'];
-  match = cookies[0].match(/\bcsrftoken=([^;]+)/);
-  csrf = (match && match[1]) || '';
-
-  response = await fetch(`${urlBase}/api/login`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'X-CSRFToken': csrf,
-      cookie: `csrftoken=${csrf}`,
-    },
-    body: JSON.stringify({
-      username,
-      password,
-    }),
-  });
-
-  console.log('login response', response.status);
-
-  const cookie = extractCookieString(response);
-  cookies = response.headers.raw()['set-cookie'];
-  match = cookies[0].match(/\bcsrftoken=([^;]+)/);
-  csrf = (match && match[1]) || '';
-
-  return {
-    'Content-Type': 'application/json',
-    'X-CSRFToken': csrf,
-    cookie,
-  };
-}
-
 export async function getAuthToken() {
   let username = process.env.DJANGO_SUPERUSER_USERNAME;
   let password = process.env.DJANGO_SUPERUSER_PASSWORD;
@@ -93,6 +29,3 @@ export async function getAuthToken() {
 
   return token;
 }
-
-
-export default getAuthInfo;
--- data/loadAllData.mjs
+++ data/loadAllData.mjs
@@ -2,14 +2,12 @@
 
 import loadLark from './lark/loadData.mjs';
 import loadLarkCampout from './lark_campout/loadData.mjs';
-import getAuthInfo, { getAuthToken } from './getAuthInfo.mjs';
+import { getAuthToken } from './getAuthInfo.mjs';
 
 main();
 
 async function main() {
-  const authInfo = await getAuthInfo();
   const authToken = await getAuthToken();
-  console.log('authInfo', authInfo);
   console.log('authToken', authToken);
   await loadLark(authToken);
   await loadLarkCampout(authToken);
