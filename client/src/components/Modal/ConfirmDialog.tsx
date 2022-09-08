5554374b65bac5418ddd6687e8c3ab0a8107c657
Will Wheeler <willfulbard@gmail.com>
2022-09-08 15:49:59 -0700

    Add deleting registrations

---
+++ client/src/components/Modal/ConfirmDialog.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Modal from './Modal';
+
+interface Props {
+  title: string,
+  onConfirm: () => any;
+  saveButtonLabel?: string,
+  message?: string;
+  onShow?: () => any;
+}
+
+class ConfirmDialog extends React.Component<Props> {
+  private modalRef = React.createRef<Modal>()
+
+  close = () => this.modalRef?.current?.close();
+  show = () => this.modalRef?.current?.show();
+  save = () => this.modalRef?.current?.save();
+
+  render() {
+    return (
+      <Modal
+        ref={this.modalRef}
+        title={this.props.title}
+        saveButtonLabel={this.props.saveButtonLabel || 'Yes'}
+        handleSave={this.props.onConfirm}
+      >{this.props.message || 'This action cannot be undone'}</Modal>
+    );
+  }
+}
+
+export default ConfirmDialog;
