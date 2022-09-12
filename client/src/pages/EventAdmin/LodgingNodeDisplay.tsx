a2cce86b08bc421c1f2c1a04ac33cd9a4f50fd56
Will Wheeler <willfulbard@gmail.com>
2022-09-12 11:23:32 -0700

    Improve prelim lodging display

--- client/src/pages/EventAdmin/LodgingNodeDisplay.tsx
+++ client/src/pages/EventAdmin/LodgingNodeDisplay.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Collapse, Button } from 'react-bootstrap';
+import { Tooltip, OverlayTrigger, Collapse, Button } from 'react-bootstrap';
 import { getCamperDisplayId } from 'utils/display';
 
 type Props = {
@@ -14,23 +14,34 @@ const defaultProps = {
 };
 
 function LodgingNodeDisplay(props: Props) {
-  const [open, setOpen] = React.useState(props.topLevel);
-  
+  const [open, setOpen] = React.useState(false);
+
+  const renderCamperList = (p: any) => props.lodgingTree.campers.length ? (
+    <Tooltip id={`camper-list-${props.lodgingTree.id}`} {...p}>
+      <ul>
+        {
+          props.lodgingTree.campers.map(c => (
+            <li>{ getCamperDisplayId(c) }</li>
+          ))
+        }
+      </ul>
+    </Tooltip>
+  ) : <div />;
+
   return (
     <div style={{ marginLeft: '2rem'}}>
-      <Button style={{ width: '100%', textAlign: 'left' }} variant="info" onClick={() => setOpen(!open)}>
-        {props.lodgingTree.name} ({props.lodgingTree.count} / {props.lodgingTree.capacity})
-      </Button>
+      <OverlayTrigger
+        placement="auto"
+        delay={{ show: 0, hide: 0 }}
+        overlay={renderCamperList}
+      >
+        <Button style={{ width: '100%', textAlign: 'left' }} variant="info" onClick={() => setOpen(!open)}>
+          {props.lodgingTree.name} ({props.lodgingTree.count} / {props.lodgingTree.capacity})
+        </Button>
+      </OverlayTrigger>
       <Collapse in={open}>
         <div>
           { props.renderNode(props.lodgingTree) }
-          <ul>
-            {
-              props.lodgingTree.campers.map(c => (
-                <li>{ getCamperDisplayId(c) }</li>
-              ))
-            }
-          </ul>
           {
             props.lodgingTree.children.map(c => (
               <LodgingNodeDisplay
