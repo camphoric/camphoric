2fc77d86f285dade4b70cf3e235949f710d19eb5
Will Wheeler <willfulbard@gmail.com>
2022-09-12 17:57:23 -0700

    Add better payment reporting

--- data/lark_campout/reports.mjs
+++ data/lark_campout/reports.mjs
@@ -12,4 +12,15 @@ export default [
 Total Campers: {{campers.length}}
 `,
   },
+  {
+    title: 'Payments',
+    template: `
+| Email | Total | Payments | Balance |
+| ----- | ----- | -------- | ------- |
+{{#eachrsort registrations "total_balance"}}
+| {{registrant_email}} | ${{total_owed}} | ${{total_payments}} | <div style="color: {{#if (lt total_balance 0)}}red{{else}}black{{/if}}">${{total_balance}}</div> |
+{{/eachrsort}}
+`,
+  },
+
 ];
