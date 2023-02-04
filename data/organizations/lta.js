219fa31d6b27d04c29ab4690db7999752a7060a5
Will Wheeler <willfulbard@gmail.com>
2023-02-04 15:05:56 -0800

    Add name of sender

--- data/organizations/lta.js
+++ data/organizations/lta.js
@@ -3,7 +3,7 @@ export default {
     name: 'Lark Traditional Arts',
   },
   emailAccount: {
-    name: 'Test gmail account',
+    name: process.env.CAMPHORIC_LTA_GMAIL_NAME,
     host: 'smtp.gmail.com',
     port: '587',
     username: process.env.CAMPHORIC_LTA_GMAIL_USERNAME,
