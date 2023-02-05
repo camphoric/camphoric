38944c131604534cc34d742d0e48dd99e68830a1
Will Wheeler <willfulbard@gmail.com>
2023-02-04 18:23:57 -0800

    Fix dates in template vars

--- data/lark/regTemplateVars.js
+++ data/lark/regTemplateVars.js
@@ -1,9 +1,9 @@
 export default {
-  first_half_camp_start_date_time: 'Fri Jul 30th at 3pm',
-  first_half_camp_end_date_time:   'Tue Aug 4th at noon',
-  second_half_camp_start_date_time: 'Tue Aug 4th at 2pm',
-  second_half_camp_end_date_time: 'Sat Aug 8th at 9am',
+  first_half_camp_start_date_time: 'Fri Jul 28th at 3pm',
+  first_half_camp_end_date_time:   'Tue Aug 1th at noon',
+  second_half_camp_start_date_time: 'Tue Aug 1th at 2pm',
+  second_half_camp_end_date_time: 'Sat Aug 5th at 9am',
 
-  full_camp_start_date_time: 'Fri Jul 30th at 3pm',
-  full_camp_end_date_time: 'Sat Aug 8th at 9am',
+  full_camp_start_date_time: 'Fri Jul 28th at 3pm',
+  full_camp_end_date_time: 'Sat Aug 5th at 9am',
 };
