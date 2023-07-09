b7f29a02e5ee4c98b9b0a2879a1f1a03296457ad
Will Wheeler <willfulbard@gmail.com>
2023-07-08 19:25:03 -0700

    Add some vite config files

---
+++ client/src/vite-env.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="vite/client" />
+/// <reference types="vite-plugin-comlink/client" />
+/// <reference lib="webworker" />
+
+declare module "*.png";
+declare module "*.svg";
+declare module "*.jpeg";
+declare module "*.jpg";
