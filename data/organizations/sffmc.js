875f671ef89412a77e3761c1effa157d0c73a268
Will Wheeler <willfulbard@gmail.com>
2023-08-27 00:56:32 -0700

    Add SFFMC to test organizations

---
+++ data/organizations/sffmc.js
@@ -0,0 +1,17 @@
+export default {
+  organization: {
+    name: 'SFFMC',
+  },
+  emailAccount: process.env.CAMPHORIC_TEST_GMAIL_USERNAME ? {
+    name: 'Test gmail account',
+    host: 'smtp.gmail.com',
+    port: '587',
+    username: process.env.CAMPHORIC_TEST_GMAIL_USERNAME,
+    password: process.env.CAMPHORIC_TEST_GMAIL_PASSWORD,
+  } : {
+    name: 'Dummy email account',
+    backend: 'django.core.mail.backends.console.EmailBackend',
+    host: 'smtp.example.com',
+    port: '587',
+  },
+};
