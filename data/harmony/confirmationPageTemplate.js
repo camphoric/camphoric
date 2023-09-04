4ef6eb42fe1f17519d6c2da4bf457f8a22f474ba
Will Wheeler <willfulbard@gmail.com>
2023-09-03 19:28:58 -0700

    Fix confirmation page text (#414)

--- data/harmony/confirmationPageTemplate.js
+++ data/harmony/confirmationPageTemplate.js
@@ -5,13 +5,12 @@ export default `
 
 ## See you at Camp Harmony ${yearDisplay}!
 
-If you're paying by check, please make it for **\${{pricing_results.total}}**
-payable to "Lark Traditional Arts", and mail it to:
+If you are paying by check, make your check for **\${{pricing_results.total}}**
+payable to SFFMC and mail to:
 
-Lark Traditional Arts
-PO Box 1724
-Mendocino, CA 95460
-USA
+SFFMC, c/o Ellen Eagan
+149 Santa Maria Avenue
+San Bruno CA 94066
 
-Do you have questions? Email us at registration@larkcamp.org or call 707-397-5275
+Questions? Email us at campnewharmony@gmail.com (preferred) or call 415-987-0502. Be patient, we are volunteers.
 `;
