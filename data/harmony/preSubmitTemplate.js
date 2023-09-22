2f36c1c53cdf17c20a40f286b133d2dc7720e0e6
Will Wheeler <willfulbard@gmail.com>
2023-09-22 13:20:10 -0700

    Fix #419 - harmony preSubmit template

--- data/harmony/preSubmitTemplate.js
+++ data/harmony/preSubmitTemplate.js
@@ -1,4 +1,3 @@
 export default `
-By submitting this form, you agree to the
-[Terms of Registration](https://docs.google.com/document/d/1pq2i2rHpHnsoB8kqtpH7FFn50JA-XELaRRMHh4hdTFg/edit?usp=sharing)
+By submitting this form, you agree to the [SFFMC Etiquette document](https://docs.google.com/document/d/1SFptLv4V84lso9yqTWT49s__W3elnUQn/edit?usp=sharing&ouid=112175546889387017762&rtpof=true&sd=true)
 `;
