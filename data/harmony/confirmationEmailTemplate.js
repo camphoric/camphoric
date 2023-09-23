ef0ac89cf5289c0b8d17986fc38e2e7cacf8e057
Will Wheeler <willfulbard@gmail.com>
2023-09-23 01:32:36 -0700

    Fix #415 - Update harmony confirmation email

--- data/harmony/confirmationEmailTemplate.js
+++ data/harmony/confirmationEmailTemplate.js
@@ -6,30 +6,42 @@ Dear {{campers.0.first_name}} {{campers.0.last_name}},
 
 Thank you for your online registration for Camp Harmony ${yearDisplay}! This email confirms the following:
 
-You have registered the following campers:
-
+Registration Info
 {{#campers}}
-- {{first_name}} {{last_name}}
-  - {{age}}
-  - Chore: {{chore}}
-  - Lodging: {{ lodging }}
-  - \${{pricing_result.total}}
+
+**{{first_name}} {{last_name}}, {{age}}**
+
+{{phone}}    
+{{email}}    
+Meals: {{meal_type}}{{#meal_exceptions}}, {{.}}{{/meal_exceptions}}    
+Registered for {{#attendance}}, {{.}}{{/attendance}}    
+Please arrive after 2pm on your first day.    
+Housing: {{lodging.lodging_requested.name}}    
+Sheet Rental: {{linens}}    
+Campership request: \${{campership_request}}    
+
+------
 {{/campers}}
 
-Donation to Lark Traditional Arts - \${{ pricing_results.donation }}
+Total fees: \${{pricing_results.tuition}}    
+Total campership donation: \${{pricing_results.campership_donation}}    
 
-TOTAL DUE: \${{pricing_results.total}}
+TOTAL FOR THIS REGISTRATION: \${{pricing_results.total}}    
 
-You chose to pay by {{registration.payment_type}}
+You have elected to pay by {{registration.payment_type}}.
 
-If you're paying by check, please make it for \${{pricing_results.total}} payable to "Lark Traditional Arts", and mail it to:
+If you are paying by check, make your check for **\${{pricing_results.total}}**
+payable to SFFMC and mail to:
 
-Lark Traditional Arts    
-PO Box 1724    
-Mendocino, CA 95460    
-USA    
+SFFMC, c/o Ellen Eagan
+149 Santa Maria Avenue
+San Bruno CA 94066
 
-Do you have questions? Email us at registration@larkcamp.org or call 707-397-5275
+If you would like to sign up to lead a workshop, you may do so on the [Workshop Signup Thread](https://docs.google.com/forms/d/e/1FAIpQLSfe9LwY1nBqNPan8QE6lcndFJsTJpnTL3fbwK1BKFNsyumEWA/viewform?vc=0&c=0&w=1&flr=0)
+
+You have agreed to Camp Harmony's Covid policy [link] and you will need to provide proof of a negative Covid test upon entering Camp.
+
+You will be receiving an email with your housing assignment and additional details by December 4. Thank you for registering for Camp Harmony!
 
 Registration Number: ${yearDisplay}CH{{ registration.id }}
 `;
