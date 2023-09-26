fa6ee56df3069bb2faac5379bf23c9fc72414a8a
Will Wheeler <willfulbard@gmail.com>
2023-09-25 17:45:52 -0700

    Fix #440 - Change text in conf email

--- data/harmony/confirmationEmailTemplate.js
+++ data/harmony/confirmationEmailTemplate.js
@@ -15,9 +15,9 @@ Registration Info
 {{email}}    
 Meals: {{meal_type}}{{#meal_exceptions}}, {{.}}{{/meal_exceptions}}    
 Registered for {{#attendance}}, {{.}}{{/attendance}}    
-Please arrive after 2pm on your first day.    
+(You must arrive after 2pm on your first day)    
 Housing: {{lodging.lodging_requested.name}}    
-Sheet Rental: {{linens}}    
+Linens rental: {{linens}}    
 Campership request: \${{campership_request}}    
 
 ------
@@ -39,9 +39,9 @@ San Bruno CA 94066
 
 Checks must be received by November 1 to receive the Early Bird rate.
 
-If you would like to sign up to lead a workshop, you may do so on the [Workshop Signup Thread](https://docs.google.com/forms/d/e/1FAIpQLSfe9LwY1nBqNPan8QE6lcndFJsTJpnTL3fbwK1BKFNsyumEWA/viewform?vc=0&c=0&w=1&flr=0)
+Each camper 2 years and older will need to take a rapid COVID test no more than 8 hours BEFORE they arrive at Camp and show proof of a negative result. There is no parking area for you to test when you arrive at Camp. Please test before you arrive. If you test positive for COVID before camp, a full refund less a $25 per-camper administration fee will be offered. Camp Harmony’s Covid & Cancellation policy is here. Free tests may be ordered through your health insurance and from https://www.covid.gov/tests. If you haven't had one already, consider getting a flu shot and Covid vaccine/booster to help insure a healthy Camp.
 
-You have agreed to Camp Harmony's Covid policy [link] and you will need to provide proof of a negative Covid test upon entering Camp.
+If you would like to sign up to lead a workshop, you may do so on the [Workshop Signup Thread](https://docs.google.com/forms/d/e/1FAIpQLSfe9LwY1nBqNPan8QE6lcndFJsTJpnTL3fbwK1BKFNsyumEWA/viewform?vc=0&c=0&w=1&flr=0)
 
 You will be receiving an email with your housing assignment and additional details by December 4. Thank you for registering for Camp Harmony!
 
