68aae6b8d4d546c8c9bf2eaa0c805e76eb56949a
Will Wheeler <willfulbard@gmail.com>
2023-11-05 16:17:36 -0800

    Filter events by organization in event chooser

--- client/src/pages/EventChooser.tsx
+++ client/src/pages/EventChooser.tsx
@@ -4,15 +4,19 @@ import { Link, useLocation, } from 'react-router-dom';
 import { Container, Row, Col } from 'react-bootstrap';
 
 import Spinner from 'components/Spinner';
-import api from 'hooks/api';
+import api, { useUrlParams } from 'hooks/api';
 
 function EventChooser() {
   const { pathname } = useLocation();
+  const { organizationId } = useUrlParams();
   const eventsApi = api.useGetEventsQuery();
 
   if (eventsApi.isFetching || eventsApi.isLoading) return <Spinner />;
+  if (!organizationId) return <Spinner />;
 
-  const events = eventsApi.data || [];
+  const events = (eventsApi.data || []).filter(
+    (e) => e.organization.toString() === organizationId
+  );
 
   return (
     <Container><Row className="justify-content-md-center"><Col>
