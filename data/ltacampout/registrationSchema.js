6f5b459f191d2a62d23b0397c1317b57262463b6
Will Wheeler <willfulbard@gmail.com>
2024-05-29 21:04:37 -0700

    Add more changes to campout registration

--- data/ltacampout/registrationSchema.js
+++ data/ltacampout/registrationSchema.js
@@ -1,8 +1,39 @@
-import { year } from './dates.js'
+import { year, dates } from './dates.js';
+
+const ordinate = (i) => {
+  const j = i % 10;
+  const k = i % 100;
+  let ordinal = 'th';
+
+  if (j === 1 && k !== 11) {
+    ordinal = 'st';
+  }
+  if (j === 2 && k !== 12) {
+    ordinal = 'nd';
+  }
+  if (j === 3 && k !== 13) {
+    ordinal = 'rd';
+  }
+
+  return `${i}${ordinal}`;
+};
+
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const dateFormat = (d) => {
+  const month = months[d.getMonth()];
+  const day = ordinate(d.getDate());
+  const dow = daysOfWeek[d.getDay()];
+
+  return `${dow} ${month} ${day}`;
+};
+
 
 export default {
   'title': `Jughandle Campout ${year} Registration`,
   'description': `
+**${dateFormat(dates.start)} to ${dateFormat(dates.end)}**    
+
 Read the [Terms of Registration](https://docs.google.com/document/d/1pq2i2rHpHnsoB8kqtpH7FFn50JA-XELaRRMHh4hdTFg/edit?usp=sharing)
 before you fill out this form.
 
