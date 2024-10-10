95f85532064774101c3d1e6d500109bb548af7e9
Will Wheeler <willfulbard@gmail.com>
2024-10-10 16:42:28 -0700

    Add pricing for apt (harmony)

--- data/harmony/pricing/pricing.js
+++ data/harmony/pricing/pricing.js
@@ -3,7 +3,7 @@
 
 export const pricingMatrix = {
   adult: {
-    apt:   { early: 204, regular: 214 },
+    apt:   { early: 211, regular: 221 },
 		lodge: { early: 204, regular: 214 },
     motel: { early: 197, regular: 207 },
     cabin: { early: 145, regular: 155 },
@@ -11,7 +11,7 @@ export const pricingMatrix = {
     tent:  { early: 130, regular: 140 },
   },
   yadult: {
-    apt:   { early: 204, regular: 214 },
+    apt:   { early: 211, regular: 221 },
     lodge: { early: 150, regular: 160 },
     motel: { early: 148, regular: 158 },
     cabin: { early: 109, regular: 119 },
@@ -19,7 +19,7 @@ export const pricingMatrix = {
     tent:  { early:  98, regular: 108 },
   },
   child: {
-    apt:   { early: 204, regular: 214 },
+    apt:   { early: 211, regular: 221 },
     lodge: { early: 100, regular: 105 },
     motel: { early:  99, regular: 104 },
     cabin: { early:  73, regular:  78 },
