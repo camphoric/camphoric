959f4259917d14070b195fc5ea3ac9f2d49f4803
Will Wheeler <willfulbard@gmail.com>
2024-10-10 16:35:16 -0700

    Add late registrant reg type (harmony)
    Fixes #519

--- data/harmony/registrationTypes.js
+++ data/harmony/registrationTypes.js
@@ -1,8 +1,31 @@
+import { dates, yearDisplay } from './dates.js';
+
+const emails = {
+  'late-registrant': () => `
+Dear {{recipient_name}},
+
+You have been invited to register late to Camp Harmony ${yearDisplay}!
+
+[Please click here to register!]({{register_link}})
+
+Questions? Email us at [campnewharmony@gmail.com](mailto:campnewharmony@gmail.com)
+(preferred) or call [415-987-0502](tel:415-987-0502). Be patient, we are volunteers.
+
+All the best,
+
+The Camp Harmony Registrar
+`,
+};
+
+const specialType = (regType) => ({
+  ...regType,
+  invitation_email_subject: `Register for ${regType.label}`,
+  invitation_email_template:
+    emails[regType.name]
+      ? emails[regType.name](regType)
+      : 'test email',
+});
+
 export default [
-  {
-    name: 'committee',
-    label: 'committee',
-    invitation_email_subject: 'Register for committee',
-    invitation_email_template: 'Test committee email',
-  }
-];
+  { name: 'late-registrant', label: 'Late Registrant' },
+].map(specialType);
