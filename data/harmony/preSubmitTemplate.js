1181bed1b28f3e161b57665911a4e6dab70f5d47
Will Wheeler <willfulbard@gmail.com>
2024-10-14 16:11:27 -0700

    Update post-submit text (harmony) #538

--- data/harmony/preSubmitTemplate.js
+++ data/harmony/preSubmitTemplate.js
@@ -1,3 +1,3 @@
 export default `
-By submitting this form, you agree to the [SFFMC Etiquette document](https://docs.google.com/document/d/1SFptLv4V84lso9yqTWT49s__W3elnUQn/edit?usp=sharing&ouid=112175546889387017762&rtpof=true&sd=true)
+By submitting this form, I affirm that I have read and agree to the [SFFMC Etiquette document](https://docs.google.com/document/d/1SFptLv4V84lso9yqTWT49s__W3elnUQn/edit?usp=sharing&ouid=112175546889387017762&rtpof=true&sd=true)
 `;
