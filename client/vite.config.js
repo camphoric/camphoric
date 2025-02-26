d75ec63ddc6529f05a39fc434aed3dd0e1e3e870
Will Wheeler <willfulbard@gmail.com>
2025-02-25 23:50:13 -0800

    Add vagrant and ansible settings

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig(() => {
     server: {
       proxy: {
         '/api': {
-          target: 'http://django:8000/',
+          target: 'http://localhost:8000/',
           changeOrigin: true,
           secure: false,      
           ws: true,
