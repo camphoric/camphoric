423ba94ecbe7490c6265e3624fc220fc12d57c2c
Will Wheeler <willfulbard@gmail.com>
2025-02-25 23:50:13 -0800

    Add vagrant and ansible settings

--- server/frontend_bootstrap/urls.py
+++ server/frontend_bootstrap/urls.py
@@ -1,6 +1,9 @@
 from django.urls import re_path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     re_path(r'^.*', views.FrontendBootstrapView.as_view())
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
