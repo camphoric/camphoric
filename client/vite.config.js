5b8d75f46b1e6dcfd88ba84c418d61b6ed960f0a
Will Wheeler <willfulbard@gmail.com>
2025-02-27 16:07:12 -0800

    Fix docker vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig(() => {
     server: {
       proxy: {
         '/api': {
-          target: 'http://localhost:8000/',
+          target: process.env.VITE_API_PROXY || 'http://localhost:8000/',
           changeOrigin: true,
           secure: false,      
           ws: true,
