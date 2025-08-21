2c66bb1fc0e8303a16bc9272429516c3022e9110
Will Wheeler <willfulbard@gmail.com>
2025-08-20 17:35:24 -0700

    Change campout pricing

--- data/ltacampout/registrationSchema.js
+++ data/ltacampout/registrationSchema.js
@@ -1,4 +1,6 @@
 import { year, dates } from './dates.js';
+import { pricingMatrix } from './pricing/pricing.js';
+
 
 const ordinate = (i) => {
   const j = i % 10;
@@ -40,12 +42,15 @@ before you fill out this form.
 Please note that registrations are accepted in the order they are received and
 your camping preferences are processed on a "first come - first served" basis.
 
-| Pricing | 26+ years old | 5-25 years old | 0-4 years old |
-| ------- | ------------- | -------------- | ------------- |
-| Shared Room | $200 | $100 | $0 |
-| Private Room | $250 | $125 | $0 |
-| RV/Tent camping | $150 | $75 | $0 |
-| Lodging Off-site | $75 | $75 | $0 |
+| Pricing | 26+ years old | 18-25 years old | 5-17 years old | 0-4 years old |
+| ------- | ------------- | --------------- | -------------- | ------------- |
+| Shared Room | $${pricingMatrix.adult.sharedroom} | $${pricingMatrix.yadult.sharedroom} | $${pricingMatrix.child.sharedroom} | $${pricingMatrix.baby.sharedroom} |
+| Private Room | $${pricingMatrix.adult.privateroom} | $${pricingMatrix.yadult.privateroom} | $${pricingMatrix.child.privateroom} | $${pricingMatrix.baby.privateroom} |
+| Premium Suite<sup>†</sup> | $${pricingMatrix.adult.premium} | $${pricingMatrix.yadult.premium} | $${pricingMatrix.child.premium} | $${pricingMatrix.baby.premium} |
+| RV/Tent camping | $${pricingMatrix.adult.tent} | $${pricingMatrix.yadult.tent} | $${pricingMatrix.child.tent} | $${pricingMatrix.baby.tent} |
+| Lodging Off-site | $${pricingMatrix.adult.offsite} | $${pricingMatrix.yadult.offsite} | $${pricingMatrix.child.offsite} | $${pricingMatrix.baby.offsite} |
+
+† Premium suites must be booked with 2 or more people
 
 Fields marked with an asterisk (*) are required.
 `,
