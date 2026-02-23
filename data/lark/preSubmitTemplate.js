bbee4370da19047ef98688f068b25f87963c8612
Will Wheeler <willfulbard@gmail.com>
2026-02-22 22:07:35 -0800

    Add Google Doc link for Lark Camp Terms of Reg

--- data/lark/preSubmitTemplate.js
+++ data/lark/preSubmitTemplate.js
@@ -1,4 +1,4 @@
 export default `
 By submitting this form, you agree to the
-[Terms of Registration](http://www.larkcamp.org/campterms.html)
+[Terms of Registration](https://docs.google.com/document/d/1c4FV7ymza8HrUKiSklJqLRaPq8wGye2psE0GNCjBI-s/edit?usp=sharing)
 `;
