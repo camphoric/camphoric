55e119f88ea3de8140ff5e3937083a0ce2ef9a29
Will Wheeler <willfulbard@gmail.com>
2026-09-24 13:00:32 -0700

    fix(harmony): fix harmony membership text
    fixes #655

--- data/harmony/registrationSchema.js
+++ data/harmony/registrationSchema.js
@@ -86,15 +86,14 @@ Fields marked with an asterisk (*) are required
     'is_member': {
       'title': 'SFFMC Membership',
       'description': `Current San Francisco Folk Music Club membership is
-required to attend Camp Harmony.  Please verify that you are a current member
-by [clicking here](https://www.sffmc.org/log-in/?redirect_to=https%3A%2F%2Fwww.sffmc.org%2Fmembership-account%2F)
-to log into the site to check your expiration or auto-renew date.  If you don't
+required to attend Camp Harmony.
+Please [verify that you are a current member by clicking here](https://www.sffmc.org/log-in/?redirect_to=https%3A%2F%2Fwww.sffmc.org%2Fmembership-account%2F)
+to log into the site to check your expiration or auto-renew date. If you don't
 know your password or haven't logged in before, click the "Lost Password" link
 to request a new password.  If you believe you are a member but are unable to
 log in even after requesting a new password, contact SFFMC membership secretary
 Ellen Eagan at [membership@sffmc.org](mailto:membership@sffmc.org) for
-assistance.  If you are new to the Folk Club, join us by clicking “become a
-member” under the “Join Us!” tab.
+assistance. If you are new to the Folk Club, [click here to join](https://www.sffmc.org/join-us/).
 `,
       'type': 'boolean',
     },
